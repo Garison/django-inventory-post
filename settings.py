fc6b91b805f99e703b17764c87594c03aa4b1b56
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2011-01-03 13:25:34 -0400

    Made path's relative and updated settings.py for latest Django version 1.2.x

--- settings.py
+++ settings.py
@@ -1,23 +1,37 @@
 # Django settings for django-inventory project.
-
 import os
+import sys
+
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "./"))
 
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
+sys.path.append(os.path.join(PROJECT_ROOT, 'modules'))
+sys.path.append(os.path.join(PROJECT_ROOT, 'customization_apps'))
+sys.path.append(os.path.join(PROJECT_ROOT, 'apps'))
+sys.path.append(os.path.join(PROJECT_ROOT, 'shared_apps'))
+sys.path.append(os.path.join(PROJECT_ROOT, '3rd_party_apps'))
 
+PROJECT_TITLE = 'Django Replication'
+PROJECT_NAME = 'django_replication'
+
+DEBUG = False
+DEVELOPMENT = False
+TEMPLATE_DEBUG = DEBUG
 ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
+        # ('Your Name', 'your_email@domain.com'),
 )
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'django-inventory.sqlite'             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(PROJECT_ROOT, "%s.sqlite" % PROJECT_NAME),     # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -29,6 +43,7 @@ TIME_ZONE = 'America/Puerto_Rico'
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'es'
+#LANGUAGE_CODE = 'en-us'
 
 ugettext = lambda s: s
 
@@ -45,7 +60,7 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = 'site_media/'
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'site_media/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -55,7 +70,7 @@ MEDIA_URL = '/site_media/'
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = MEDIA_URL + 'admin_media/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '4#8p2r=zfa=t@3@@v5v*)i=kvnuyxae_yyvqkyv4!opcwad6@+'
@@ -67,23 +82,31 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.load_template_source',
 )
 
-MIDDLEWARE_CLASSES = (
+#MIDDLEWARE_CLASSES = (
+#    'django.middleware.common.CommonMiddleware',
+#    'django.contrib.sessions.middleware.SessionMiddleware',
+#    'django.middleware.locale.LocaleMiddleware',
+#    'django.contrib.auth.middleware.AuthenticationMiddleware',
+#)
+MIDDLEWARE_CLASSES = [
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-)
+    'django.contrib.messages.middleware.MessageMiddleware',
+]
 
-ROOT_URLCONF = 'django-inventory.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    './templates',
+    'templates',
 )
 
-INSTALLED_APPS = (
+INSTALLED_APPS = [
+    #'grappelli',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -92,15 +115,18 @@ INSTALLED_APPS = (
     'django-inventory.inventory',
     'django.contrib.admindocs',
     'wsgi',
-)
+]
 
-TEMPLATE_CONTEXT_PROCESSORS = (
-        "django.core.context_processors.auth",
-        "django.core.context_processors.debug",
-        "django.core.context_processors.i18n",
-        "django.core.context_processors.media",
-        'django.core.context_processors.request',
-)
+TEMPLATE_CONTEXT_PROCESSORS = [
+    "django.core.context_processors.auth",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    'django.core.context_processors.request',
+#    "grappelli.context_processors.admin_template_path",
+    'django.contrib.messages.context_processors.messages',
+]
+
+GRAPPELLI_ADMIN_TITLE = PROJECT_TITLE
 
 LOGIN_URL = '/login/'
 
@@ -110,12 +136,37 @@ AUTHENTICATION_BACKENDS = (
 
 CUSTOM_USER_MODEL = 'inventory.CustomUser'
 
-DEVELOPMENT=True
+try:
+    from settings_local import *
+except ImportError:
+    pass
+
 
 if DEVELOPMENT:
+    INTERNAL_IPS = ('127.0.0.1',)
     try:
-        from settings_development import *
-#		print u'Starting development version.'
-    except: # ImportError:
-        pass
-    #    print u'File settings_development.py is not found.'
+        import rosetta
+        INSTALLED_APPS.append('rosetta')
+    except ImportError:
+        print "rosetta is not installed"
+
+    try:
+        import django_extensions
+        INSTALLED_APPS.append('django_extensions')
+    except ImportError:
+        print "django_extensions is not installed"
+
+    try:
+        import debug_toolbar
+#        INSTALLED_APPS.append('debug_toolbar')
+    except ImportError:
+        print "debug_toolbar is not installed"
+
+    TEMPLATE_CONTEXT_PROCESSORS.append("django.core.context_processors.debug")
+
+    WSGI_AUTO_RELOAD = True
+    if 'debug_toolbar' in INSTALLED_APPS:
+        MIDDLEWARE_CLASSES.append('debug_toolbar.middleware.DebugToolbarMiddleware')
+        DEBUG_TOOLBAR_CONFIG={
+            'INTERCEPT_REDIRECTS' : False,
+        }
