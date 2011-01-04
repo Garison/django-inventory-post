5683a923ed861f6f14658a0065247dfffc04fa71
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2011-01-04 13:15:55 -0400

    Enable rosetta and include all models for the moment

--- apps/inventory/admin.py
+++ apps/inventory/admin.py
@@ -1,8 +1,22 @@
-from inventory.models import *
 from django.contrib import admin
 
-mysite = admin.AdminSite()
-mysite.register(CustomUser)
-mysite.register(Permission)
+from inventory.models import CustomUser, Permission, RegionalOffice, \
+                             Department, Supply, ItemTemplate, Item, \
+                             ItemGroup, RetiredItem, InRepairsItem, \
+                             Person, Inventory
+
+
+admin.site.register(CustomUser)
+admin.site.register(Permission)
+admin.site.register(RegionalOffice)
+admin.site.register(Department)
+admin.site.register(Supply)
+admin.site.register(ItemTemplate)
+admin.site.register(Item)
+admin.site.register(ItemGroup)
+admin.site.register(RetiredItem)
+admin.site.register(InRepairsItem)
+admin.site.register(Person)
+admin.site.register(Inventory)
 
 
