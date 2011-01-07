efaec14366f3d112a260609faaffca3ee6e678b1
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2011-01-07 02:13:20 -0400

    Added templatetag to insert all available states into the context

---
+++ apps/inventory/templatetags/inventory_tags.py
@@ -0,0 +1,24 @@
+from django.template.defaultfilters import stringfilter
+from django.template import Library, Node, Variable
+from django.core.urlresolvers import reverse
+
+from inventory.models import State
+
+register = Library()
+
+
+
+class GetAllStatesNode(Node):
+    def __init__(self, variable):
+        self.variable = variable
+
+    def render(self, context):
+        context[self.variable] = State.objects.all()
+        return ''
+
+@register.tag
+def get_all_states(parser, token):
+    args = token.contents.split()
+    if len(args) != 3 or args[1] != 'as':
+        raise TemplateSyntaxError("'get_all_states' requires 'as variable' (got %r)" % args)
+    return GetAllStatesNode(args[2])
