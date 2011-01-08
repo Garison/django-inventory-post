4f9c8d7939bf69a845eeee0b58e2b8262f806c97
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2011-01-08 04:50:13 -0400

    Further html skin conversion updates

--- apps/inventory/__init__.py
+++ apps/inventory/__init__.py
@@ -76,7 +76,7 @@ navigation = [
     {'text':_('Users'), 'view':'person_list', 'links':user_menu_links},
     {'text':_('Templates'), 'view':'template_list', 'links':template_menu_links},
     {'text':_('Assets'), 'view':'item_list', 'links':item_menu_links},
-#    {'text':_('Assets states'), 'view':'', 'links':item_states_menu_links},
+    {'text':_('Assets states'), 'view':''},#, 'links':item_states_menu_links},
     {'text':_('Inventories'), 'view':'inventory_list', 'links':inventory_menu_links},
     {'text':_('Inventory transactions'), 'view':'inventory_transaction_list', 'links':inventory_transactions_menu_links},
     {'text':_('Tools'), 'view':'location_list', 'links':tools_menu_links},
