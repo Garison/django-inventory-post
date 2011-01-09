c610814157dc52bfcfd1ddc01c0ae3de5c6a5233
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2011-01-09 15:59:17 -0400

    Reorganized menus

--- apps/inventory/__init__.py
+++ apps/inventory/__init__.py
@@ -3,16 +3,19 @@ from django.utils.translation import ugettext_lazy as _
 
 from models import Location, State
 
-person_menu_links = [
-    {'text':_('View all'), 'view':'person_list'},
-    {'text':_('Create new'), 'view':'person_create'},
-]
+person_list = {'text':_('View all people'), 'view':'person_list'}
+person_create = {'text':_('Create new person'), 'view':'person_create'}
+person_update = {'text':_(u'Edit'), 'view':'person_update'}
+person_delete = {'text':_(u'Delete'), 'view':'person_delete'}
+person_photos = {'text':_(u'Edit photos'), 'view':'person_photos'}
+person_assign_item = {'text':_(u'Assign/Remove asset'), 'view':'person_assign_item'}
+
+asset_list = {'text':_('View all assets'), 'view':'item_list'}
+asset_create = {'text':_('Create new asset'), 'view':'item_create'}
+asset_orphan_list = {'text':_('Orphans assets'), 'view':'item_orphans_list'}
 
 person_links = [
-    {'text':_(u'Edit'), 'view':'person_update', 'icon':settings.MEDIA_URL + 'images/accessories-text-editor.png'},
-    {'text':_(u'Delete'), 'view':'person_delete', 'icon':settings.MEDIA_URL + 'images/emblem-unreadable.png'},
-    {'text':_(u'Edit photos'), 'view':'person_photos', 'icon':settings.MEDIA_URL + 'images/camera-photo.png' },
-    {'text':_(u'Assign/Remove asset'), 'view':'person_assign_item', 'icon':settings.MEDIA_URL + 'images/item-plus-user.png'},
+    person_update, person_delete, person_photos, person_assign_item
 ]
 
 template_menu_links = [
@@ -30,32 +33,35 @@ template_record_links = [
 ]
 
 item_menu_links = [
-    {'text':_('View all'), 'view':'item_list'},
-    {'text':_('Create new'), 'view':'item_create'},
-    {'text':_('Orphans'), 'view':'item_orphans_list'},
+    asset_list, asset_create, asset_orphan_list   
 ]
 
 item_record_links = [
-    {'text':_(u'Edit'), 'view':'item_update', 'icon':settings.MEDIA_URL + 'images/accessories-text-editor.png'},
-    {'text':_(u'Delete'), 'view':'item_delete', 'icon':settings.MEDIA_URL + 'images/emblem-unreadable.png'},
-    {'text':_(u'Photos'), 'view':'item_photos', 'icon':settings.MEDIA_URL + 'images/camera-photo.png'},
-    {'text':_(u'Assign/Remove people'), 'view':'item_assign_person', 'icon':settings.MEDIA_URL + 'images/item-plus-user.png'},
-    {'text':_(u'Template'), 'view':'template_view', 'icon':settings.MEDIA_URL + 'images/font-x-generic.png', 'args':'object.item_template.id'},
+    {'text':_(u'Edit'), 'view':'item_update'},
+    {'text':_(u'Delete'), 'view':'item_delete'},
+    {'text':_(u'Photos'), 'view':'item_photos'},
+    {'text':_(u'Assign/Remove people'), 'view':'item_assign_person'},
+    {'text':_(u'Template'), 'view':'template_view'},
 ]
+
+
+group_list = {'text':_(u'View all groups'), 'view':'group_list'}
+group_create = {'text':_(u'Create group'), 'view':'group_create'}
+group_update = {'text':_(u'Edit'), 'view':'group_update'}
+group_delete = {'text' : _(u'Delete'), 'view':'group_delete'}
 
 group_links = [
-    {'text':_(u'Edit'), 'view':'group_update', 'icon':settings.MEDIA_URL + 'images/accessories-text-editor.png'},
-    {'text' : _(u'Delete'), 'view':'group_delete', 'icon':settings.MEDIA_URL + 'images/emblem-unreadable.png'},
+    group_update, group_delete
 ]
 
-group_menu_links = [
-    {'text':_(u'View all'), 'view':'group_list'},
-    {'text':_(u'Create'), 'view':'group_create'},
-]
+inventory_list = {'text':_('View all inventories'), 'view':'inventory_list'}
+inventory_create = {'text':_('Create new inventory'), 'view':'inventory_create'}
+
+inventory_transaction_list = {'text':_('View all transactions'), 'view':'inventory_transaction_list'}
+inventory_transaction_create = {'text':_('Create new transaction'), 'view':'inventory_transaction_create'}
 
 inventory_menu_links = [
-    {'text':_('View all'), 'view':'inventory_list'},
-    {'text':_('Create new'), 'view':'inventory_create'},
+    inventory_list, inventory_create, inventory_transaction_list, inventory_transaction_create
 ]
 
 inventory_links = [
@@ -64,10 +70,6 @@ inventory_links = [
     {'text':_(u'Delete'), 'view':'inventory_delete', 'icon':settings.MEDIA_URL + 'images/emblem-unreadable.png'},
 ]
 
-inventory_transactions_menu_links = [
-    {'text':_('View all'), 'view':'inventory_transaction_list'},
-    {'text':_('Create new'), 'view':'inventory_transaction_create'},
-]
 
 inventory_transaction_links = [
     {'text':_(u'Edit'), 'view':'inventory_transaction_update', 'icon':settings.MEDIA_URL + 'images/accessories-text-editor.png'},
@@ -82,13 +84,15 @@ tools_menu_links = [
 
 navigation = [
     {'text':_('Home'), 'view':'home', 'famfam':'house'},
-    {'text':_('Users'), 'view':'person_list', 'links':person_menu_links, 'famfam':'group'},
+    {'text':_('People'), 'view':'person_list', 'links':[
+        person_list, person_create
+    ]},
     {'text':_('Templates'), 'view':'template_list', 'links':template_menu_links, 'famfam':'page'},
-    {'text':_('Assets'), 'view':'item_list', 'links':item_menu_links},
-    {'text':_('Asset groups'), 'view':'group_list', 'links':group_menu_links},
+    {'text':_('Assets'), 'view':'item_list', 'links':[
+        asset_list, asset_create, asset_orphan_list, group_list, group_create 
+    ]},
     {'text':_('Asset states'), 'view':'item_state_list_init'},
     {'text':_('Inventories'), 'view':'inventory_list', 'links':inventory_menu_links,'famfam':'book'},
-    {'text':_('Inventory transactions'), 'view':'inventory_transaction_list', 'links':inventory_transactions_menu_links,'famfam':'book_open'},
     {'text':_('Tools'), 'view':'location_list', 'links':tools_menu_links,'famfam':'wrench'},
     {'text':_('Search'), 'view':'search', 'famfam':'zoom'},
     {'text':_('About'), 'view':'about'},
