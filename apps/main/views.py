3dfe046e492740aa233e2806a7314bd565c60aaa
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2011-01-18 21:30:08 -0400

    Changed home template to show some art and eliminated statistics

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,15 +1,6 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-from inventory.models import Person, Item, ItemTemplate
-
-
 def home(request):
-    data =  {
-        'person':Person.objects.all(),
-        'item':Item.objects.all(),
-        'template':ItemTemplate.objects.all(),
-    }
-
-    return render_to_response('home.html', data,
+    return render_to_response('home.html', {},
     context_instance=RequestContext(request))     
