5827cb66c980344ef45ffc4271be3db8a28aea42
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2011-01-19 00:46:56 -0400

    Changed link texts to all lowercase

--- apps/assets/__init__.py
+++ apps/assets/__init__.py
@@ -1,42 +1,42 @@
 from django.utils.translation import ugettext_lazy as _
 
-state_list = {'text':_('States'), 'view':'state_list'}
-state_edit = {'text':_(u'Edit'), 'view':'state_update'}
-state_delete = {'text':_(u'Delete'), 'view':'state_delete'}
+state_list = {'text':_('states'), 'view':'state_list'}
+state_edit = {'text':_(u'edit'), 'view':'state_update'}
+state_delete = {'text':_(u'delete'), 'view':'state_delete'}
 
 state_record_links = [
     state_edit, state_delete
 ]
 
-person_list = {'text':_('View all people'), 'view':'person_list'}
-person_create = {'text':_('Create new person'), 'view':'person_create'}
-person_update = {'text':_(u'Edit'), 'view':'person_update'}
-person_delete = {'text':_(u'Delete'), 'view':'person_delete'}
-person_photos = {'text':_(u'Edit photos'), 'view':'person_photos'}
-person_assign_item = {'text':_(u'Assign/Remove asset'), 'view':'person_assign_item'}
+person_list = {'text':_('view all people'), 'view':'person_list'}
+person_create = {'text':_('create new person'), 'view':'person_create'}
+person_update = {'text':_(u'edit'), 'view':'person_update'}
+person_delete = {'text':_(u'delete'), 'view':'person_delete'}
+person_photos = {'text':_(u'edit photos'), 'view':'person_photos'}
+person_assign_item = {'text':_(u'assign/remove asset'), 'view':'person_assign_item'}
 
 person_record_links = [
     person_update, person_delete, person_photos, person_assign_item
 ]
 
-asset_list = {'text':_('View all assets'), 'view':'item_list'}
-asset_create = {'text':_('Create new asset'), 'view':'item_create'}
-asset_orphan_list = {'text':_('Orphans assets'), 'view':'item_orphans_list'}
-asset_edit = {'text':_(u'Edit'), 'view':'item_update'}
-asset_delete = {'text':_(u'Delete'), 'view':'item_delete'}
-asset_photos = {'text':_(u'Photos'), 'view':'item_photos'}
-asset_assign_person = {'text':_(u'Assign/Remove people'), 'view':'item_assign_person'}
-asset_template = {'text':_(u'Template'), 'view':'template_view', 'args':'object.item_template.id'}
+asset_list = {'text':_('view all assets'), 'view':'item_list'}
+asset_create = {'text':_('create new asset'), 'view':'item_create'}
+asset_orphan_list = {'text':_('orphans assets'), 'view':'item_orphans_list'}
+asset_edit = {'text':_(u'edit'), 'view':'item_update'}
+asset_delete = {'text':_(u'delete'), 'view':'item_delete'}
+asset_photos = {'text':_(u'photos'), 'view':'item_photos'}
+asset_assign_person = {'text':_(u'assign/remove people'), 'view':'item_assign_person'}
+asset_template = {'text':_(u'template'), 'view':'template_view', 'args':'object.item_template.id'}
 
 
 asset_record_links = [
     asset_edit, asset_delete, asset_photos, asset_assign_person, asset_template
 ]
 
-group_list = {'text':_(u'View all groups'), 'view':'group_list'}
-group_create = {'text':_(u'Create group'), 'view':'group_create'}
-group_update = {'text':_(u'Edit'), 'view':'group_update'}
-group_delete = {'text' : _(u'Delete'), 'view':'group_delete'}
+group_list = {'text':_(u'view all groups'), 'view':'group_list'}
+group_create = {'text':_(u'create group'), 'view':'group_create'}
+group_update = {'text':_(u'edit'), 'view':'group_update'}
+group_delete = {'text' : _(u'delete'), 'view':'group_delete'}
 
 group_record_links = [
     group_update, group_delete
--- apps/inventory/__init__.py
+++ apps/inventory/__init__.py
@@ -4,36 +4,34 @@ from models import Location
 import assets
 
 
-inventory_list = {'text':_('View all inventories'), 'view':'inventory_list'}
-inventory_create = {'text':_('Create new inventory'), 'view':'inventory_create'}
+inventory_list = {'text':_('view all inventories'), 'view':'inventory_list'}
+inventory_create = {'text':_('create new inventory'), 'view':'inventory_create'}
 
-inventory_transaction_list = {'text':_('View all transactions'), 'view':'inventory_transaction_list'}
-inventory_transaction_create = {'text':_('Create new transaction'), 'view':'inventory_transaction_create'}
+inventory_transaction_list = {'text':_('view all transactions'), 'view':'inventory_transaction_list'}
+inventory_transaction_create = {'text':_('create new transaction'), 'view':'inventory_transaction_create'}
 
-location_list = {'text':_('Locations'), 'view':'location_list'}
+location_list = {'text':_('locations'), 'view':'location_list'}
 
-supplier_create = {'text':_('Create new supplier'), 'view':'supplier_create'}
-supplier_list = {'text':_('Suppliers'), 'view':'supplier_list'}
-supplier_delete = {'text':_('Delete'), 'view':'supplier_delete'}
-supplier_update = {'text':_('Edit'), 'view':'supplier_update'}
-supplier_assign_itemtemplate = {'text':_(u'Assign/Remove templates'), 'view':'supplier_assign_itemtemplates'}
+supplier_create = {'text':_('create new supplier'), 'view':'supplier_create'}
+supplier_list = {'text':_('suppliers'), 'view':'supplier_list'}
+supplier_delete = {'text':_('delete'), 'view':'supplier_delete'}
+supplier_update = {'text':_('edit'), 'view':'supplier_update'}
+supplier_assign_itemtemplate = {'text':_(u'assign/remove templates'), 'view':'supplier_assign_itemtemplates'}
 
-template_assign_suppliers = {'text':_(u'Assign/Remove suppliers'), 'view':'template_assign_suppliers'}
-
-
+template_assign_suppliers = {'text':_(u'assign/remove suppliers'), 'view':'template_assign_suppliers'}
 
 template_menu_links = [
-    {'text':_('View all'), 'view':'template_list'},
-    {'text':_('Create new'), 'view':'template_create'},
-    {'text':_('Orphans'), 'view':'template_orphans_list'},
+    {'text':_('view all'), 'view':'template_list'},
+    {'text':_('create new'), 'view':'template_create'},
+    {'text':_('orphans'), 'view':'template_orphans_list'},
 ]
 
 template_record_links = [
-    {'text':_(u'Edit'), 'view':'template_update'},
-    {'text':_(u'Delete'), 'view':'template_delete'},
-    {'text':_(u'Photos'), 'view':'template_photos'},
-    {'text':_(u'Assets that use this template'), 'view':'template_items_list'},
-    {'text':_(u'Assign/Remove supplies'), 'view':'template_assign_supply'},
+    {'text':_(u'edit'), 'view':'template_update'},
+    {'text':_(u'delete'), 'view':'template_delete'},
+    {'text':_(u'photos'), 'view':'template_photos'},
+    {'text':_(u'assets that use this template'), 'view':'template_items_list'},
+    {'text':_(u'assign/remove supplies'), 'view':'template_assign_supply'},
     template_assign_suppliers
 ]
 
@@ -44,33 +42,33 @@ inventory_menu_links = [
 
 
 inventory_links = [
-    {'text':_(u'Current balances'), 'view':'inventory_current'},
-    {'text':_(u'Edit'), 'view':'inventory_update'},
-    {'text':_(u'Delete'), 'view':'inventory_delete'},
+    {'text':_(u'current balances'), 'view':'inventory_current'},
+    {'text':_(u'edit'), 'view':'inventory_update'},
+    {'text':_(u'delete'), 'view':'inventory_delete'},
 ]
 
 
 inventory_transaction_links = [
-    {'text':_(u'Edit'), 'view':'inventory_transaction_update'},
-    {'text':_(u'Delete'), 'view':'inventory_transaction_delete'},
+    {'text':_(u'edit'), 'view':'inventory_transaction_update'},
+    {'text':_(u'delete'), 'view':'inventory_transaction_delete'},
 ]
 
 navigation = [
-    {'text':_('Home'), 'view':'home', 'famfam':'house'},
-    {'text':_('People'), 'view':'person_list', 'famfam':'group', 'links':[
+    {'text':_('home'), 'view':'home', 'famfam':'house'},
+    {'text':_('people'), 'view':'person_list', 'famfam':'group', 'links':[
         assets.person_list, assets.person_create
     ]},
-    {'text':_('Templates'), 'view':'template_list', 'links':template_menu_links, 'famfam':'page'},
-    {'text':_('Assets'), 'view':'item_list', 'links':[
+    {'text':_('templates'), 'view':'template_list', 'links':template_menu_links, 'famfam':'page'},
+    {'text':_('assets'), 'view':'item_list', 'links':[
         assets.asset_list, assets.asset_create, assets.asset_orphan_list, assets.group_list, assets.group_create 
     ]},
-    {'text':_('Asset states'), 'view':'item_state_list_init'},
-    {'text':_('Inventories'), 'view':'inventory_list', 'links':inventory_menu_links,'famfam':'book'},
-    {'text':_('Tools'), 'view':'location_list', 'links': [
+    {'text':_('asset states'), 'view':'item_state_list_init'},
+    {'text':_('inventories'), 'view':'inventory_list', 'links':inventory_menu_links,'famfam':'book'},
+    {'text':_('tools'), 'view':'location_list', 'links': [
         location_list, assets.state_list, supplier_list
     ],'famfam':'wrench'},
-    {'text':_('Search'), 'view':'search', 'famfam':'zoom'},
-    {'text':_('About'), 'view':'about'},
+    {'text':_('search'), 'view':'search', 'famfam':'zoom'},
+    {'text':_('about'), 'view':'about'},
 ]
 
 location_filter = {
@@ -78,8 +76,8 @@ location_filter = {
 }
 
 location_links = [
-    {'text':_(u'Edit'), 'view':'location_update'},
-    {'text':_(u'Delete'), 'view':'location_delete'},
+    {'text':_(u'edit'), 'view':'location_update'},
+    {'text':_(u'delete'), 'view':'location_delete'},
 ]
 
 
