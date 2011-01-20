5e091ffc028f8e828413359b43c97692b43f8404
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2011-01-20 16:42:14 -0400

    ImportWizard can now handle formsets better

--- apps/importer/api.py
+++ apps/importer/api.py
@@ -1,1 +1,72 @@
+import os
 import csv
+
+from django.utils.translation import ugettext_lazy as _
+from django.utils.encoding import smart_unicode
+
+def perform_import(csvfilename, model, mappings, remove_top=None, dryrun=True):
+    csvfile = open(csvfilename, 'rb')
+    dialect = csv.Sniffer().sniff(csvfile.read(1024))
+    csvfile.seek(0)
+    
+    try:
+        csvfile = file(csvfilename, 'r')
+    except IOError:
+        self.error(_(u'Could not open specified csv file, %s, or it does not exist') % datafile, 0)
+    else:
+        # CSV Reader returns an iterable, but as we possibly need to
+        # perform list commands and since list is an acceptable iterable, 
+        # we'll just transform it.
+        csvfile = list(csv.reader(csvfile, dialect=dialect))
+
+    if remove_top:
+        self.csvfile.pop(0)
+
+
+    #model_fields = dict([(f.name,f) for f in self.model._meta.fields])
+    model_fields = model._meta.init_name_map()
+    imported_lines = 0
+    errors = 0
+    results = []
+    line = 1
+    for column in csvfile:
+        column = [smart_unicode(c) for c in column]
+        model_line = {}
+        line_error = False
+        for field_exp in mappings:
+            if field_exp['enabled']:
+                try:
+                    field = model_fields[field_exp['model_field']][0]
+
+                    expression = eval(field_exp['expression'], {'csv_column':column})
+                    if hasattr(field, 'related'):
+                        arguments = {field_exp['arguments']:expression}
+                        value = field.related.parent_model.objects.get(**arguments)
+                    else:
+                        value = expression
+
+                    model_line[field_exp['model_field']] = value
+
+                except Exception, err:
+                    value = None
+                    line_error = True
+                    errors += 1
+                    results.append(_(u'Foreign key fetch error, line: %s, expression: %s, error: %s') % (line, expression, err))
+        try:
+            if not line_error:
+                if dryrun:
+                    entry = model(**model_line)
+                else:
+                    entry = model.objects.create(**model_line)
+
+                imported_lines += 1
+        except Exception, err:
+            errors += 1
+            results.append(_(u'Import error, line: %s, error: %s') % (line, err))
+        
+        line += 1
+    
+    results.append(_(u'Imported %s lines') % imported_lines)
+    results.append(_(u'There were %s errors') % errors)
+    
+    return results
--- apps/importer/views.py
+++ apps/importer/views.py
@@ -28,7 +28,7 @@ def handle_uploaded_file(f):
 def import_wizard(request):
     ExpressionFormSet = formset_factory(ExpressionForm, extra=0)
     ExpressionFormSet.title = _(u'expressions')
-    wizard = ImportWizard(form_list=[PreviewForm, ExpressionFormSet, ImportResultForm])
+    wizard = ImportWizard(form_list=[PreviewForm, ExpressionFormSet, ImportResultForm, ImportResultForm])
     return wizard(request)
     
    
@@ -55,66 +55,6 @@ def import_file(request):
     },
     context_instance=RequestContext(request))    
    
-    
-'''
-def import_file(request):
-    title = ''
-    hidden_fields = {}
-    formset = None
-    step = int(request.POST.get('step', 1))
-    if request.method == 'POST':
-        if step == 1:
-            title = _(u'Upload a file to import')
-            form = DocumentForm(request.POST, request.FILES)
-            if form.is_valid():
-                title = _(u'File preview')
-                hidden_fields['step'] = 2               
-                hidden_fields['model_name'] = form.cleaned_data['model_name']                
-                temp_file = handle_uploaded_file(form.cleaned_data['local_document'])
-                form = prepare_preview_form(temp_file)
-                hidden_fields['temp_file'] = temp_file
-
-        if step == 2:
-            temp_file = request.POST.get('temp_file', None)
-            model_name = request.POST.get('model_name', None)
-            app_label, name = model_name.split('.')
-            ct = ContentType.objects.get(app_label=app_label, name=name)
-            model = ct.model_class()
-            ExpressionFormSet = formset_factory(ExpressionForm, extra=0)
-            initial=[]
-            for num, field in zip(range(len(model._meta.fields)), model._meta.fields):
-                if field.name != 'id':
-                    initial.append({
-                        'model_field':field.name,
-                        'expression':'"%%s" %% row.column[%s]' % str(num-1),
-                        'enabled':(field.name != 'id' and True)
-                        })
-            formset = ExpressionFormSet(initial=initial)
-            form = None
-            hidden_fields['step'] = 3
-            hidden_fields['model_name'] = model_name
-            hidden_fields['temp_file'] = temp_file
-        if step == 3:
-            temp_file = request.POST.get('temp_file', None)
-            model_name = request.POST.get('model_name', None)
-            app_label, name = model_name.split('.')
-            ct = ContentType.objects.get(app_label=app_label, name=name)
-            model = ct.model_class()
-            perform_import(model, temp_file)
-            
-    else:
-        title = _(u'Upload a file to import')
-        form = DocumentForm()
-        hidden_fields['step'] = 1
-
-    return render_to_response('generic_form.html', {
-        'hidden_fields':hidden_fields,
-        'form':form,
-        'formset':formset,
-        'title':title,
-    },
-    context_instance=RequestContext(request))    
-'''
 '''
 
 def prepare_preview_form(filepath):
