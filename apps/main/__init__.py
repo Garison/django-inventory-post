800cad1779c1a2fcf3477f802c1f7ad6193f81e4
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2011-01-24 11:18:04 -0400

    Cleanups

--- apps/main/__init__.py
+++ apps/main/__init__.py
@@ -6,19 +6,6 @@ import movements
 
 from assets.models import Item, Person, ItemGroup, State
 from inventory.models import ItemTemplate, Inventory, InventoryTransaction, Supplier, Location
-
-#object_navigation = {
-#    Item:[assets.asset_edit, assets.asset_delete, assets.asset_photos, assets.asset_assign_person, assets.asset_template],
-#    Person:[assets.person_update, assets.person_delete, assets.person_photos, assets.person_assign_item],
-#    ItemGroup:[assets.group_update, assets.group_delete],
-#    State:[assets.state_edit, assets.state_delete],
-#    ItemTemplate:[inventory.template_update, inventory.template_delete, inventory.template_photos, inventory.template_assets, inventory.template_assign_supplies, inventory.template_assign_suppliers],
-#    Supplier:[inventory.supplier_update, inventory.supplier_delete, inventory.supplier_assign_itemtemplate],
-#    Inventory:[inventory.inventory_balances, inventory.inventory_update, inventory.inventory_delete],
-#    InventoryTransaction:[inventory.inventory_transaction_update, inventory.inventory_transaction_delete],
-#    Location:[inventory.location_update, inventory.location_delete],
-#}
-
     
 menu_navigation = [
     {'text':_('home'), 'view':'home', 'famfam':'house'},
