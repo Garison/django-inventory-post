643b964f7ed69deac6dba2b8292600d2cc4a0ca8
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2011-01-24 11:15:40 -0400

    Cleanups and added missing icons for Purchase Request states

--- apps/movements/__init__.py
+++ apps/movements/__init__.py
@@ -1,10 +1,12 @@
 from django.utils.translation import ugettext_lazy as _
 
-purchase_request_state_list = {'text':_('purchase request states'), 'view':'purchase_request_state_list'}#, 'famfam':'package_go'}
-purchase_request_state_create = {'text':_('create new state'), 'view':'purchase_request_state_create'}#, 'famfam':'package_go'}
-purchase_request_state_update = {'text':_('edit state'), 'view':'purchase_request_state_update'}#, 'famfam':'package_go'}
-purchase_request_state_delete = {'text':_('delete state'), 'view':'purchase_request_state_delete'}#, 'famfam':'package_go'}
+from common.api import register_links
 
-purchase_request_state_record_links = [
-    purchase_request_state_update, purchase_request_state_delete
-    ]
+from models import PurchaseRequestStatus
+
+purchase_request_state_list = {'text':_('purchase request states'), 'view':'purchase_request_state_list', 'famfam':'pencil_go'}
+purchase_request_state_create = {'text':_('create new state'), 'view':'purchase_request_state_create', 'famfam':'pencil_add'}
+purchase_request_state_update = {'text':_('edit state'), 'view':'purchase_request_state_update', 'args':'object.id', 'famfam':'pencil'}
+purchase_request_state_delete = {'text':_('delete state'), 'view':'purchase_request_state_delete', 'args':'object.id', 'famfam':'pencil_delete'}
+
+register_links(PurchaseRequestStatus, [purchase_request_state_update, purchase_request_state_delete])
