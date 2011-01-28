2269d3b03d537abb6fd8b57e1bbd0617ee7dee0e
Roberto Rosario <roberto.rosario.gonzalez@gmail.com>
2011-01-28 11:08:53 -0400

    changed to gettext_lazy to translate strings at runtime

--- apps/main/__init__.py
+++ apps/main/__init__.py
@@ -1,4 +1,4 @@
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 
 import assets
 import inventory
@@ -10,16 +10,16 @@ from assets.models import Item, Person, ItemGroup, State
 from inventory.models import ItemTemplate, Inventory, InventoryTransaction, Supplier, Location
     
 register_menu([
-    {'text':_('home'), 'view':'home', 'famfam':'house', 'position':0},
+    {'text':_(u'home'), 'view':'home', 'famfam':'house', 'position':0},
 
-    {'text':_('tools'), 'view':'location_list', 'links': [
+    {'text':_(u'tools'), 'view':'location_list', 'links': [
         inventory.location_list, assets.state_list, 
         inventory.supplier_list,
         movements.purchase_request_state_list, 
         movements.purchase_order_state_list,
         movements.purchase_order_item_state_list,
-        {'text':_('import'), 'view':'import_wizard', 'famfam':'lightning_add'},
+        {'text':_(u'import'), 'view':'import_wizard', 'famfam':'lightning_add'},
     ],'famfam':'wrench', 'name':'tools','position':5},
     
-    {'text':_('about'), 'view':'about', 'position':7},
+    {'text':_(u'about'), 'view':'about', 'position':7},
 ])
